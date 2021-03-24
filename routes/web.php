752cf433acdf49405c77e507397c92e965491eef
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-03-24 22:31:37 +0800

    Ep 8 : Store Blog Posts as HTML Files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(!file_exists($path)) {
+        return back();
+    }
+
+    $post = file_get_contents($path);
+
+
+    return view('post', [
+        'post' => $post
+    ]);
+});
