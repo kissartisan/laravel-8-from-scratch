97a10e7da5c8f8dee3b13205ca3da60c03817e0f
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-04-02 21:30:57 +0800

    Ep. 15 : Blade Layouts First Way : extend & yield

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <article>
          <h1>{{ $post->title }}</h1>
 
@@ -16,5 +10,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,30 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@section('content')
     @foreach($posts as $post)
-    @dd($loop)
-    <article>
-        <h1>
-            <a href="/post/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
+        <article>
+            <h1>
+                <a href="/post/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
 
-        <div>
-            {{ $post->excerpt  }}
-        </div>
-    </article>
-    @endforeach
+            <div>
+                {{ $post->excerpt  }}
+            </div>
+        </article>
+        @endforeach
 
-    <a href="/">Go Back</a>
-</body>
-
-</html>
+        <a href="/">Go Back</a>
+@endsection
