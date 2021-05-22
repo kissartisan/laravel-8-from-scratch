8bad8d456ccea5d925eefcb90251b8247c25c388
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-05-22 11:17:18 +0800

    Ep. 30 : Eager Load Relationships on an Existing Model

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     ]);
 });
 
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
+
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
