39c6aba89554acef907b7b29540502d4dfc92608
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-05-23 12:57:04 +0800

    Ep. 31 : Convert the HTML and CSS to Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,39 @@
 <x-layout>
-    @foreach($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
+    @include('_posts-header')
 
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
 
-        <div>
-            {{ $post->excerpt  }}
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
         </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+
+{{-- @foreach($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+    {{ $post->title }}
+    </a>
+    </h1>
+
+    <p>
+        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
+        <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+    </p>
+
+    <div>
+        {{ $post->excerpt  }}
+    </div>
     </article>
     @endforeach
 
-    <a href="/">Go Back</a>
+    <a href="/">Go Back</a> --}}
 </x-layout>
