1f8287edf80914384e5a9c822b64ac66bb6df362
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-07-15 23:31:24 +0800

    Ep. 32 : Blade Components and CSS Grids > Part 1

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -8,21 +8,23 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="#"
+                    <a href="/categories/{{ $post->category->slug }}"
                         class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">Techniques</a>
-                    <a href="#"
+                        style="font-size: 10px">{{ $post->category->name }}</a>
+                    <a href=""
                         class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
                         style="font-size: 10px">Updates</a>
                 </div>
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                        <a href="/posts/{{ $post->slug }}">
+                            {{ $post->title }}
+                        </a>
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                        Published <time>1 day ago</time>
+                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
                     </span>
                 </div>
             </header>
@@ -45,7 +47,7 @@
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
-                        <h5 class="font-bold">Lary Laracore</h5>
+                        <h5 class="font-bold">{{ $post->author->name }}</h5>
                         <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,18 +2,25 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post" />
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p>No posts yet. Please check back later.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-3">
+        {{-- <div class="lg:grid lg:grid-cols-3">
             <x-post-card />
             <x-post-card />
             <x-post-card />
-        </div>
+        </div> --}}
     </main>
 
 {{-- @foreach($posts as $post)
