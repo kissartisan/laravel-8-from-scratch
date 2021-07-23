b9de565ee972f937c9eb6ebe969636f6a4ab568e
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-07-23 20:16:03 +0800

    Ep. 32 : Blade Components and CSS Grids > Part 2

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,5 +1,7 @@
+@props(['post'])
+
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    {{ $attributes->merge(['class' => "transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"]) }}>
     <div class="py-6 px-5">
         <div>
             <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,15 +3,7 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
-            <x-post-featured-card :post="$posts[0]" />
-
-            @if ($posts->count() > 1)
-                <div class="lg:grid lg:grid-cols-2">
-                    @foreach ($posts->skip(1) as $post)
-                        <x-post-card :post="$post" />
-                    @endforeach
-                </div>
-            @endif
+            <x-posts-grid :posts="$posts" />
         @else
             <p>No posts yet. Please check back later.</p>
         @endif
