618ecea9ef06c1f913cd9678670e95a272ff6b42
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-07-24 19:48:15 +0800

    Ep. 33 : Convert the Blog Post Page

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -10,12 +10,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="/categories/{{ $post->category->slug }}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category->name }}</a>
-                    <a href=""
-                        class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">Updates</a>
+                    <x-category-button :category="$post->category" />
                 </div>
 
                 <div class="mt-4">
