c7558342efc0aa71a6de3c05f9bfd4a697fc3642
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-08-07 11:38:21 +0800

    Ep. 34 : A Small JavaScript Dropdown Detour

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,9 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine-ie11.min.js"
+    integrity="sha512-Atu8sttM7mNNMon28+GHxLdz4Xo2APm1WVHwiLW9gW4bmHpHc/E2IbXrj98SmefTmbqbUTOztKl5PDPiu0LD/A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,8 @@ Route::get('/', function () {
     // });
 
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -35,12 +36,15 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load('category', 'author')
+        'posts' => $author->posts->load('category', 'author'),
+        'categories' => Category::all()
     ]);
 });
