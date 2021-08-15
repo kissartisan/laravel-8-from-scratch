cc8dcac43a4cceb8254f1f9231e19720c24c1e6c
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-08-15 19:48:48 +0800

    Ep. 38 : Search (The Cleaner Way)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,13 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, $filters)
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+            $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,29 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if(request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-
-    // DB::listen(function ($query) {
-    //     logger($query->sql, $query->bindings);
-    // });
-
-    return view('posts', [
-        'posts' => $posts->get(), //Post::latest()->with('category', 'author')->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('/posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
