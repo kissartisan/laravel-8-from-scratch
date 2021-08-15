e40cc4e17423e566706ce4a8aa9f913efa56abe8
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-08-15 19:15:36 +0800

    Ep. 36 : Quick Tweaks and Clean-Up

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -30,15 +30,8 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-2">
-                <p>
-                    {{ $post->excerpt }}
-                </p>
-
-                <p class="mt-4">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                    nulla pariatur.
-                </p>
+            <div class="text-sm mt-2 space-y-4">
+                {!! $post->excerpt !!}
             </div>
 
             <footer class="flex justify-between items-center mt-8">
